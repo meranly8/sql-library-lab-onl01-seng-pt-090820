9482d1fa7bca6203dd8d7a10be208c8b8e5a7f67
Michelle Ranly <meranly8@gmail.com>
2020-11-10 08:37:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,9 @@ def select_series_title_with_most_human_characters
   "SELECT series.title, COUNT(species)
    FROM series
    JOIN characters
-    ON series.author_id = characters.author_id"
+    ON series.author_id = characters.author_id
+   WHERE characters.species = 'human'
+   ORDER BY COUNT(species)"
 end
 
 def select_character_names_and_number_of_books_they_are_in
