86f1c4dbc6ae17652f77f5668e8a0699a4688a10
Michelle Ranly <meranly8@gmail.com>
2020-11-10 08:29:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) AS species_count
    FROM characters
-   GROUP BY species_count DESC LIMIT 1;"
+   GROUP BY species_count LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
